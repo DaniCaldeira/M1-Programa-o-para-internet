0278c296e93426e385e6b4c66a84cdeeb38b6a8e
Dani Caldeira <daniellecaldeirasp@hotmail.com>
2024-02-20 11:32:28 -0300

    Implementando autentificação

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import process from 'process';
 import path from 'path';
+import session from "express-session";
+import autenticar from "./segurança/autenticar.js";
 
 const host = '0.0.0.0';
 const porta = 3000; 
 
 const app = express();
 
+app.use(session({
+    secret: 'M1nH4Ch4v3S3cr3t4',
+    resave: false,
+    saveUninitializet: true,
+    cookie:{
+        maxAge: 60 * 1000 * 15,
+    }
+}))
+
 app.use(express.static(path.join(process.cwd(), 'publico')));
 
-app.use(express.static(path.join(process.cwd(), 'privado')))
+app.use(autenticar, express.static(path.join(process.cwd(), 'privado')))
 
 app.listen(porta, host, ()=>{
     console.log(`Servidor escutando em http://${host}:${porta}`);
